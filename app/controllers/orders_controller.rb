8fae61b2ada4f882cda04335ca78071bfa28f0e2
Jun5572 <takizawa.webcamp@gmail.com>
2018-07-12 06:59:40 +0000

    add_colums

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,16 @@
+class OrdersController < ApplicationController
+  def select_address
+  end
+
+  def create_address
+  end
+
+  def confirm
+  end
+
+  def create
+  end
+
+  def show
+  end
+end
---
+++ db/migrate/20180712051101_create_orders.rb
@@ -0,0 +1,13 @@
+class CreateOrders < ActiveRecord::Migration[5.2]
+  def change
+    create_table :orders do |t|
+      t.integer :user_id
+      t.integer :address_id
+      t.integer :total
+      t.integer :peyment
+      t.integer :status
+
+      t.timestamps
+    end
+  end
+end
