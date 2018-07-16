10c80886a611f0e44b7531212a87ddead849d9c1
kazukitakahasi <kazuki.takahashiyola@gmail.com>
2018-07-16 22:20:45 +0000

    first commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -22,10 +22,6 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_in, keys:[:email,:encrypted_password,:first_name,:last_name,:kana_first,:kana_last,:postal_code,:prefecture,:city,:address1,:address2,:phone_number])
   end
 
-  def after_sign_up_path_for(resource)
-  	mypage_user_path(current_user)
-  end
-
   def after_sign_in_path_for(resource)
   	mypage_user_path(current_user)
   end
--- app/models/product.rb
+++ app/models/product.rb
@@ -3,4 +3,6 @@ class Product < ApplicationRecord
 	has_many :order_items
 	has_many :orders, through: :order_items
 	accepts_nested_attributes_for :discs, allow_destroy: true
+	belongs_to :category
+	attachment :image_url
 end
