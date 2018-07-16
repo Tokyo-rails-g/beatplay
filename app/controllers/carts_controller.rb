dbae2eadebe487ff6236031af376d63304809c83
Jun5572 <takizawa.webcamp@gmail.com>
2018-07-16 10:52:14 +0000

    takizawa

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -8,16 +8,29 @@ class CartsController < ApplicationController
   def show
     @cart_item = Cart.find(params[:id])
     @user_id = current_user_id
-
   end
 
   def delete_item
+    @cart_item = Cart.find(params[:id])
+    # カート内に商品があるかを確認。ない場合はdestroyしない
+    if Cart.exists?(id: params[:id])
+      @cart_item.destroy
+    else
+      flash[:notice] = "カートは既に空です"
+      render action: :show
   end
 
   def delete_all_items
+    @cart_items = Cart.all
+    if Cart.exists?(id: params[:id])
+      @cart_item.destroy
+    else
+      flash[:notice] = "カートは既に空です"
+      render action: :show
   end
-
+# 購入手続きに進むボタンを押した時に呼ばれるアクション
   def checkout
+    redirect_to orders_address_select_path(current_user.id)
   end
 
 
--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,17 +1,53 @@
 class OrdersController < ApplicationController
+
   def select_address
+    @user = current_user
+    @address = Address.new
   end
 
+# 新たに送り先を指定したとき
   def create_address
+    @address = Address.new(address_params)
+    if @address.save
+      # 購入確認画面にリダイレクト
+      redirect_to orders_confirm_path(@order.id)
+    end
   end
 
+# 購入確認画面
   def confirm
+    # @cart.id = current_cart.id
+    # @cart_item = CartItem.find(params[:id])
+    # @address = Address.find(address_params)
+    @user = current_user
   end
 
+# 注文を確定するボタンが押されたときに呼ばれるアクション
   def create
+    @order = Order.new
+    @order_item = OrderItem.new(order_item_params)
+    @cart_item = CartItem.find(params[:id])
+    @user_id = current_user.id
+    if @order.save
+      @order.id = order_item.id
+      @order_item.product_id = cart_item.product_id
+      @cart_item.destroy
+    end
   end
 
 # 購入完了画面で使用するアクション
   def show
+  end
+
+
+
+  private
+
+  def address_params
+    params.require(:address).permit(:first_name, :last_name, :postal_code, :prefecture, :city, :address1, :address2)
+  end
+
+  def order_item_params
+    params.require(:order_item).permit(:order_id, :product_id, :price, :quantity)
   end
 end
